8e4a9cbbb58de8147881252066799f424071e85a
pawelos <paweloski@gmail.com>
2023-01-19 13:52:45 +0100

    adding implementation

---
+++ ConsoleApp1Core/BaseCalculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1Core
+{
+	class BaseCalculator
+	{
+		int Add(int value1, int value2)
+		{
+			return value1 + value2;
+		}
+
+		int Substruct(int value1, int value2)
+		{
+			return value1 - value2;
+		}
+
+		int Multiply(int value1, int value2)
+		{
+			return value1 * value2;
+		}
+
+		int Devide(int value1, int value2)
+		{
+			return value1 / value2;
+		}
+	}
+}
--- ConsoleApp1Core/Program.cs
+++ ConsoleApp1Core/Program.cs
@@ -6,7 +6,31 @@ namespace ConsoleApp1Core
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Console.WriteLine("kalkulator");
+
+			var input = Console.ReadLine();
+			int a = Int32.Parse(input);
+			input = Console.ReadLine();
+			int b = Int32.Parse(input);
+			int wynik;
+
+			wynik = a + b;
+			Console.WriteLine("Dodawanie: {0} + {1} = {2}",a,b,wynik);
+
+			wynik = a - b;
+			Console.WriteLine("Odejmowanie: {0} - {1} = {2}", a, b, wynik);
+
+			wynik = a * b;
+			Console.WriteLine("Mnozenie: {0} * {1} = {2}", a, b, wynik);
+
+
+			wynik = a / b;
+			Console.WriteLine("Dzielenie: {0} / {1} = {2}", a, b, wynik);
+			
+
+			// obiektowo
+			// Base Calculator - metody
+
 		}
 	}
 }
