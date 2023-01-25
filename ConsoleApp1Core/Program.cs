38a6f159945bcc294cb1b1e2a655e2bac4931ad7
pawelos <paweloski@gmail.com>
2023-01-25 18:33:43 +0100

    ok

--- ConsoleApp1Core/Program.cs
+++ ConsoleApp1Core/Program.cs
@@ -6,26 +6,7 @@ namespace ConsoleApp1Core
 {
 	class Program
 	{
-		public static void CalculatorProc(int a, int b)
-		{
-			Console.WriteLine("-----  Calculator Proc ----- ");
-			int wynik = a + b;
-			Console.WriteLine("Dodawanie: {0} + {1} = {2}", a, b, wynik);
-			wynik = a - b;
-			Console.WriteLine("Odejmowanie: {0} - {1} = {2}", a, b, wynik);
-			wynik = a * b;
-			Console.WriteLine("Mnozenie: {0} * {1} = {2}", a, b, wynik);
-			wynik = a / b;
-			Console.WriteLine("Dzielenie: {0} / {1} = {2}", a, b, wynik);
-		}
-
-		public static int Silnia(int i)
-		{
-			if (i < 1)
-				return 1;
-			else
-				return i * Silnia(i - 1);
-		}
+		
 
 		static void Main(string[] args)
 		{
@@ -33,7 +14,6 @@ namespace ConsoleApp1Core
 			int a = Int32.Parse(Console.ReadLine());
 			Console.WriteLine("Podaj b:");
 			int b = Convert.ToInt32(Console.ReadLine());
-			CalculatorProc(a, b); 
 			AdvancedCalculaor ac = new AdvancedCalculaor();
 			Console.WriteLine("Silnia = {0}", ac.GetFactorial(3));
 		}
